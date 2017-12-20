a5d810893afe5c37a5e4a80392ebdb8b0ac61550
Sunki-min <minc407@gmail.com>
2017-12-20 16:03:35 +0900

    회원가입 -> 메인창

--- app/src/main/java/puzzle/zoomoneyapptest/SignUpActivity.java
+++ app/src/main/java/puzzle/zoomoneyapptest/SignUpActivity.java
@@ -1,7 +1,10 @@
 package puzzle.zoomoneyapptest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button btn_check_sign_up = (Button)findViewById(R.id.btn_check_sign_up);
+        btn_check_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go_to_sigIn = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(go_to_sigIn);
+            }
+        });
 
 
     }
