b617084a55afd1eba5decc8ce3cc6aae250c2045
Sunki-min <minc407@gmail.com>
2018-01-26 16:51:50 +0900

    layout 바꾸는 중

---
+++ app/src/main/java/puzzle/zoomoneyapptest/StartActivity.java
@@ -0,0 +1,26 @@
+package puzzle.zoomoneyapptest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                Intent i = new Intent(StartActivity.this, SignInActivity.class);
+                startActivity(i);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }, 2200);
+
+    }
+}
